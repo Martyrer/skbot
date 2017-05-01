b4a27128eaebf5ce6d14d0e2ade6e91e85a2bc12
dmitry shumilin <d.shumilin@levi9.com>
2017-05-01 19:38:32 +0300

    test

--- ws.php
+++ ws.php
@@ -6,7 +6,56 @@ use function Aerys\root;
 /** @var Router $router */
 $router = (new Router())
     ->route("POST", "/", function(Request $request, Response $response) {
-        $response->end('It Works!');
+        $body = yield $request->getBody();
+        $requestBody = json_decode($body, true);
+        $response->setStatus(200);
+        $response->end();
+
+        $serviceUrl = $requestBody['serviceUrl'];
+        $conversationId = $requestBody['conversation']['id'];
+
+        $url = $serviceUrl . '/v3/conversations/' . $conversationId . '/activities';
+        $query = http_build_query(
+            [
+                'grant_type' => 'client_credentials',
+                'client_id' => '014e516a-4287-4ba0-818b-0ed97a6a61e0',
+                'client_secret' => 'fOFffbhtcmWLxzuCurcTaTw',
+                'scope' => 'https://api.botframework.com/.default',
+            ]
+        );
+
+        $curl = curl_init();
+        curl_setopt_array($curl, [
+            CURLOPT_URL => 'https://login.microsoftonline.com/botframework.com/oauth2/v2.0/token',
+            CURLOPT_POST => true,
+            CURLOPT_POSTFIELDS => $query,
+            CURLOPT_RETURNTRANSFER => true,
+        ]);
+
+        $result = curl_exec($curl);
+        curl_close($curl);
+
+        $token = json_decode($result, true)['access_token'];
+
+        $curl = curl_init();
+        curl_setopt_array($curl, [
+            CURLOPT_URL => $url,
+            CURLOPT_POST => true,
+            CURLOPT_POSTFIELDS => json_encode([
+                'type' => 'message',
+                'text' => 'Hello World!',
+            ]),
+            CURLOPT_HTTPHEADER => [
+                'Accept' => 'application/json, text/json',
+                'Content-Type' => 'application/x-www-form-urlencoded',
+                'Authorization' => 'Bearer ' . $token,
+            ],
+            CURLOPT_RETURNTRANSFER => true,
+        ]);
+
+        curl_exec($curl);
+        curl_close($curl);
+        unset($curl);
     });
 
 $root = root(__DIR__ . "/web");
