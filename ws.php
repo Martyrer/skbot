08c16edab2039e504a0255d464ca09eb92ab95e7
dmitry shumilin <d.shumilin@levi9.com>
2017-05-01 22:18:25 +0300

    test

--- ws.php
+++ ws.php
@@ -8,33 +8,41 @@ $router = (new Router())
     ->route("POST", "/", function(Request $request, Response $response) {
         $body = yield $request->getBody();
         $requestBody = json_decode($body, true);
+        $token = '';
 
+        if ($token == '') {
+            $query = http_build_query(
+                [
+                    'grant_type' => 'client_credentials',
+                    'client_id' => '014e516a-4287-4ba0-818b-0ed97a6a61e0',
+                    'client_secret' => 'fOFffbhtcmWLxzuCurcTaTw',
+                    'scope' => 'https://api.botframework.com/.default',
+                ]
+            );
+
+            $curl = curl_init();
+            curl_setopt_array(
+                $curl,
+                [
+                    CURLOPT_URL => 'https://login.microsoftonline.com/botframework.com/oauth2/v2.0/token',
+                    CURLOPT_POST => true,
+                    CURLOPT_POSTFIELDS => $query,
+                    CURLOPT_RETURNTRANSFER => true,
+                ]
+            );
+
+            $result = curl_exec($curl);
+
+            curl_close($curl);
+            unset($curl);
+
+            $token = json_decode($result, true)['access_token'];
+        }
 
         $serviceUrl = $requestBody['serviceUrl'];
         $conversationId = $requestBody['conversation']['id'];
 
         $url = $serviceUrl . '/v3/conversations/' . $conversationId . '/activities';
-        $query = http_build_query(
-            [
-                'grant_type' => 'client_credentials',
-                'client_id' => '014e516a-4287-4ba0-818b-0ed97a6a61e0',
-                'client_secret' => 'fOFffbhtcmWLxzuCurcTaTw',
-                'scope' => 'https://api.botframework.com/.default',
-            ]
-        );
-
-        $curl = curl_init();
-        curl_setopt_array($curl, [
-            CURLOPT_URL => 'https://login.microsoftonline.com/botframework.com/oauth2/v2.0/token',
-            CURLOPT_POST => true,
-            CURLOPT_POSTFIELDS => $query,
-            CURLOPT_RETURNTRANSFER => true,
-        ]);
-
-        $result = curl_exec($curl);
-        curl_close($curl);
-
-        $token = json_decode($result, true)['access_token'];
 
         $curl = curl_init();
         curl_setopt_array($curl, [
@@ -43,10 +51,13 @@ $router = (new Router())
             CURLOPT_POSTFIELDS => json_encode([
                 'type' => 'message',
                 'text' => 'Hello World!',
+                'from' => [
+                    'name' => 'Bot',
+                ]
             ]),
             CURLOPT_HTTPHEADER => [
                 'Accept' => 'application/json, text/json',
-                'Content-Type' => 'application/x-www-form-urlencoded',
+                'Content-Type' => 'application/json',
                 'Authorization' => 'Bearer ' . $token,
             ],
             CURLOPT_RETURNTRANSFER => true,
@@ -57,7 +68,7 @@ $router = (new Router())
         unset($curl);
 
         $response->setStatus(200);
-        $response->end();
+        $response->end(json_encode(['id' => 'sdfdfsddsdfwe']));
     });
 
 $root = root(__DIR__ . "/web");
